3b97fad1a8f1cf735bbe8adacf0c5c97fd36e3b8
Kian Hodaee <kianhod@gmail.com>
2025-04-03 23:04:04 -0500

    removed usertest.java

--- backend/testFiles/UserTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
-public class UserTest {
-    public void testLogin() {
-        User user = new User("username", "password");
-        assertEquals("Success: Redirected to home page", user.login("username", "password"));
-    }
-}
